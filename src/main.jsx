4087f55417a84b5861b492ce6c97aa7590e1ef93
Avinash Yadav <avinashkumaryadav832@gmail.com>
2025-02-14 10:29:19 -0500

    editied code main and app

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom"; // Wrap with Router here
 import App from "./App.jsx";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-import "react-toastify/dist/ReactToastify.css";
-
 import { Provider } from "react-redux";
 import store from "./app/store.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+      <Router>
+        {" "}
+        {/* Only wrap here */}
         <App />
+      </Router>
     </Provider>
   </StrictMode>
 );
