cce84f2e3b75944e7cfd5dca7492dd73eb33bb23
Avinash Yadav <avinashkumaryadav832@gmail.com>
2025-02-14 10:03:08 -0500

    issue: fixed main.jsx vercel.json and vite.config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -13,7 +14,9 @@ import store from "./app/store.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
