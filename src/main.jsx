bec6b99448e1667380a99b7db44d963797d1a209
Avinash Yadav <avinashkumaryadav832@gmail.com>
2025-02-14 12:53:34 -0500

    fix issue: header

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom"; // Wrap with Router here
+import "./index.css";
 import App from "./App.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "react-toastify/dist/ReactToastify.css";
+
 import { Provider } from "react-redux";
 import store from "./app/store.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </Provider>
   </StrictMode>
 );
