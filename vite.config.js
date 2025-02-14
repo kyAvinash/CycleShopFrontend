43d80e421b9dfcdeee3fbb12c995aeb3e26ddef8
Avinash Yadav <avinashkumaryadav832@gmail.com>
2025-02-14 10:15:42 -0500

    roleback: vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Important for Vercel
+  base: "/",
+  server: {
+    historyApiFallback: true, // Ensures routing works in development
+  },
   build: {
     outDir: "dist",
   },
